0aeae358114cf60dd8cbcb6b18a92df0b3bb1c9d
gurubaladhinesh <gurubaladhinesh@gmail.com>
2024-06-04 23:41:18 +0530

    day7

--- day7/hello-world.go
+++ day7/hello-world.go
@@ -1,9 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	//Day7 - June4, 2024
 	//Functions
+	fmt.Println("1+2=", plus(1,2))
+
+	fmt.Println("1+2+3.14=", plusPlus(1,2,3.14))
 
 
+	//Multiple return value functions
+
+}
+
+
+func plus(x int, y int) int {
+	return x + y
+}
+
+func plusPlus(x,y int, z float32) int {
+	return x +y + int(z)
 }
