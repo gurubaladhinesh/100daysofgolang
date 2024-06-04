1e40d101aebd141156136c787f962ca2c80e3892
gurubaladhinesh <gurubaladhinesh@gmail.com>
2024-06-04 23:55:16 +0530

    day7

--- day7/hello-world.go
+++ day7/hello-world.go
@@ -12,7 +12,27 @@ func main() {
 
 
 	//Multiple return value functions
+	a,b,c := vals()
+	fmt.Println("received both values: ", a,b,c)
 
+	x,_,z:= vals()
+	fmt.Println("received only one value: ",x,z)
+
+
+	//Variadic functions
+	//function can be called with any number of arguments. fmt.Println() is a avariadic function
+	fmt.Println("sum of multiple numbers: ", sums(1,2,3,4,5,6,7,8,9))
+
+
+
+}
+
+func sums(nums ...int) int{
+	sum:=0
+	for _,num := range nums {
+		sum+=num
+	}
+	return sum
 }
 
 
@@ -22,4 +42,8 @@ func plus(x int, y int) int {
 
 func plusPlus(x,y int, z float32) int {
 	return x +y + int(z)
+}
+
+func vals() (int, int, int) {
+	return 3,5,9
 }
